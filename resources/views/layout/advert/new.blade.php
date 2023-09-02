1256dbf26869552b287191b1563c0b74b9fb568e
Hamdi Kalaycı <hamdikalayci@gmail.com>
2023-09-02 17:08:55 +0300

    description eklendi

--- resources/views/layout/advert/new.blade.php
+++ resources/views/layout/advert/new.blade.php
@@ -166,7 +166,7 @@
 
                     <div class="col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                            <textarea id="description" name="description"></textarea>
+                        <textarea id="description" name="description"></textarea>
 
                     </div>
 
--- resources/views/layout/advert/script/script-save.blade.php
+++ resources/views/layout/advert/script/script-save.blade.php
@@ -2,8 +2,6 @@
     $("#advertSaveBtn").on("click", function(){
         var formData = $("#advertForm").serialize();
 
-        console.log(formData);
-
         axios.post('{{ route($name) }}', formData)
             .then((res)=>{
             toastr[res.data.type](res.data.message);
